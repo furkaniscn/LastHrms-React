e6d2e88a5fd376df3d983a70d6b40cc8e624fa69
furkaniscn <furkan.iscn@outlook.com>
2021-09-20 13:31:12 +0300

    AddedVerificationAndDetails

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -12,6 +12,8 @@ import {
 } from "semantic-ui-react";
 import CandidateService from "../services/CandidateService";
 import { useFormik } from "formik";
+import { toast } from "react-toastify";
+import { useSelector } from "react-redux";
 
 export default function Register() {
 
@@ -41,9 +43,13 @@ export default function Register() {
     },
     validationSchema: candidateRegisterSchema,
     onSubmit: (values) => {
-      console.log(values)
-      candidateService.registerCandidate(values).then((result) => alert(result.message))
-      history.push("/login")
+      candidateService.registerCandidate(values).then((result) => {
+        toast.success(result.data.message)
+        history.push("/login")
+      })
+        .catch((result) => {
+          toast.error(result.response.data.message)
+        })
     }
   });
 
@@ -51,184 +57,197 @@ export default function Register() {
     formik.setFieldValue(fieldName, value);
   }
 
+  const { authItem } = useSelector(state => state.auth)
+
   return (
     <div>
-      <Header as="h2" color="teal" textAlign="center">
-        <Image src="https://hrms.ph/img/logo-large.png" /> Kayıt Ol
-      </Header>
-      <Form size="large" onSubmit={formik.handleSubmit}>
-        <Segment stacked>
-          <Grid stackable>
-            <Grid.Column width={8}>
-              <div style={{ marginTop: "1em" }}>
-                <label><b>İsim</b></label>
+      {authItem[0].loggedIn === true &&
+        <div>
+          <Message negative>
+            <Message.Header>Zaten giriş yapmış durumdasınız.</Message.Header>
+            <p>İsterseniz çıkış yapıp tekrar deneyebilirsiniz.</p>
+          </Message>
+        </div>
+      }
+      {authItem[0].loggedIn === false &&
+        <div>
+          <Header as="h2" color="teal" textAlign="center">
+            <Image src="https://hrms.ph/img/logo-large.png" /> Kayıt Ol
+          </Header>
+          <Form size="large" onSubmit={formik.handleSubmit}>
+            <Segment stacked>
+              <Grid stackable>
+                <Grid.Column width={8}>
+                  <div style={{ marginTop: "1em" }}>
+                    <label><b>İsim</b></label>
 
-                <Form.Input
-                  fluid
-                  icon="user"
-                  iconPosition="left"
-                  placeholder="İsim"
-                  type="text"
-                  value={formik.values.firstName}
-                  name="firstName"
-                  onChange={formik.handleChange}
-                  onBlur={formik.handleBlur}
-                />
-                {
-                  formik.errors.firstName && formik.touched.firstName && (
-                    <div className={"ui pointing red basic label"}>
-                      {formik.errors.firstName}
-                    </div>
-                  )
-                }
-              </div>
-              <div style={{ marginTop: "1em" }}>
-                <label><b>Soy İsim</b></label>
-                <Form.Input
-                  fluid
-                  icon="user"
-                  iconPosition="left"
-                  placeholder="Soy isim"
-                  type="text"
-                  value={formik.values.lastName}
-                  name="lastName"
-                  onChange={formik.handleChange}
-                  onBlur={formik.handleBlur}
-                />
-                {formik.errors.lastName && formik.touched.lastName && (
-                  <div className={"ui pointing red basic label"}>
-                    {formik.errors.lastName}
+                    <Form.Input
+                      fluid
+                      icon="user"
+                      iconPosition="left"
+                      placeholder="İsim"
+                      type="text"
+                      value={formik.values.firstName}
+                      name="firstName"
+                      onChange={formik.handleChange}
+                      onBlur={formik.handleBlur}
+                    />
+                    {
+                      formik.errors.firstName && formik.touched.firstName && (
+                        <div className={"ui pointing red basic label"}>
+                          {formik.errors.firstName}
+                        </div>
+                      )
+                    }
                   </div>
-                )}
-              </div>
-              <div style={{ marginTop: "1em" }}>
-                <label><b>Kimlik Numarası</b></label>
-                <Form.Input
-                  fluid
-                  icon="id card"
-                  iconPosition="left"
-                  placeholder="Kimlik numarası"
-                  type="text"
-                  value={formik.values.nationalNumber}
-                  name="nationalNumber"
-                  onChange={formik.handleChange}
-                  onBlur={formik.handleBlur}
-                />
-                {formik.errors.nationalNumber && formik.touched.nationalNumber && (
-                  <div className={"ui pointing red basic label"}>
-                    {formik.errors.nationalNumber}
+                  <div style={{ marginTop: "1em" }}>
+                    <label><b>Soy İsim</b></label>
+                    <Form.Input
+                      fluid
+                      icon="user"
+                      iconPosition="left"
+                      placeholder="Soy isim"
+                      type="text"
+                      value={formik.values.lastName}
+                      name="lastName"
+                      onChange={formik.handleChange}
+                      onBlur={formik.handleBlur}
+                    />
+                    {formik.errors.lastName && formik.touched.lastName && (
+                      <div className={"ui pointing red basic label"}>
+                        {formik.errors.lastName}
+                      </div>
+                    )}
                   </div>
-                )}
-              </div>
-              <div style={{ marginTop: "1em" }}>
-                <label><b>Doğum Tarihi</b></label>
-                <Form.Input
-                  fluid
-                  icon="calendar times"
-                  iconPosition="left"
-                  placeholder="Dogum tarihi"
-                  type="date"
-                  error={Boolean(formik.errors.birthDate)}
-                  onChange={(event, data) =>
-                    handleChangeSemantic(data.value, "birthDate")
-                  }
-                  value={formik.values.birthDate}
-                  onBlur={formik.handleBlur}
-                  name="birthDate"
-                />
-                {formik.errors.birthDate && formik.touched.birthDate && (
-                  <div className={"ui pointing red basic label"}>
-                    {formik.errors.birthDate}
+                  <div style={{ marginTop: "1em" }}>
+                    <label><b>Kimlik Numarası</b></label>
+                    <Form.Input
+                      fluid
+                      icon="id card"
+                      iconPosition="left"
+                      placeholder="Kimlik numarası"
+                      type="text"
+                      value={formik.values.nationalNumber}
+                      name="nationalNumber"
+                      onChange={formik.handleChange}
+                      onBlur={formik.handleBlur}
+                    />
+                    {formik.errors.nationalNumber && formik.touched.nationalNumber && (
+                      <div className={"ui pointing red basic label"}>
+                        {formik.errors.nationalNumber}
+                      </div>
+                    )}
                   </div>
-                )}
-              </div>
-            </Grid.Column>
+                  <div style={{ marginTop: "1em" }}>
+                    <label><b>Doğum Tarihi</b></label>
+                    <Form.Input
+                      fluid
+                      icon="calendar times"
+                      iconPosition="left"
+                      placeholder="Dogum tarihi"
+                      type="date"
+                      error={Boolean(formik.errors.birthDate)}
+                      onChange={(event, data) =>
+                        handleChangeSemantic(data.value, "birthDate")
+                      }
+                      value={formik.values.birthDate}
+                      onBlur={formik.handleBlur}
+                      name="birthDate"
+                    />
+                    {formik.errors.birthDate && formik.touched.birthDate && (
+                      <div className={"ui pointing red basic label"}>
+                        {formik.errors.birthDate}
+                      </div>
+                    )}
+                  </div>
+                </Grid.Column>
 
-            <Grid.Column width={8}>
-              <div style={{ marginTop: "1em" }}>
-                <label><b>Email</b></label>
-                <Form.Input
-                  fluid
-                  icon="mail"
-                  iconPosition="left"
-                  placeholder="E-mail adresi"
-                  type="email"
-                  value={formik.values.email}
-                  onChange={formik.handleChange}
-                  onBlur={formik.handleBlur}
-                  name="email"
-                />
-                {formik.errors.email && formik.touched.email && (
-                  <div className={"ui pointing red basic label"}>
-                    {formik.errors.email}
+                <Grid.Column width={8}>
+                  <div style={{ marginTop: "1em" }}>
+                    <label><b>Email</b></label>
+                    <Form.Input
+                      fluid
+                      icon="mail"
+                      iconPosition="left"
+                      placeholder="E-mail adresi"
+                      type="email"
+                      value={formik.values.email}
+                      onChange={formik.handleChange}
+                      onBlur={formik.handleBlur}
+                      name="email"
+                    />
+                    {formik.errors.email && formik.touched.email && (
+                      <div className={"ui pointing red basic label"}>
+                        {formik.errors.email}
+                      </div>
+                    )}
                   </div>
-                )}
-              </div>
-              <div style={{ marginTop: "1em" }}>
-                <label><b>Email Tekrar</b></label>
-                <Form.Input
-                  fluid
-                  icon="mail"
-                  iconPosition="left"
-                  placeholder="E-mail adresi tekrar"
-                  onChange={formik.handleChange}
-                  onBlur={formik.handleBlur}
-                  name="reEmail"
-                />
-                {formik.errors.reEmail && formik.touched.reEmail && (
-                  <div className={"ui pointing red basic label"}>
-                    {formik.errors.reEmail}
+                  <div style={{ marginTop: "1em" }}>
+                    <label><b>Email Tekrar</b></label>
+                    <Form.Input
+                      fluid
+                      icon="mail"
+                      iconPosition="left"
+                      placeholder="E-mail adresi tekrar"
+                      onChange={formik.handleChange}
+                      onBlur={formik.handleBlur}
+                      name="reEmail"
+                    />
+                    {formik.errors.reEmail && formik.touched.reEmail && (
+                      <div className={"ui pointing red basic label"}>
+                        {formik.errors.reEmail}
+                      </div>
+                    )}
                   </div>
-                )}
-              </div>
-              <div style={{ marginTop: "1em" }}>
-                <label><b>Şifre</b></label>
-                <Form.Input
-                  fluid
-                  icon="lock"
-                  iconPosition="left"
-                  placeholder="Şifre"
-                  type="password"
-                  value={formik.values.password}
-                  onChange={formik.handleChange}
-                  onBlur={formik.handleBlur}
-                  name="password"
-                />
-                {formik.errors.password && formik.touched.password && (
-                  <div className={"ui pointing red basic label"}>
-                    {formik.errors.password}
+                  <div style={{ marginTop: "1em" }}>
+                    <label><b>Şifre</b></label>
+                    <Form.Input
+                      fluid
+                      icon="lock"
+                      iconPosition="left"
+                      placeholder="Şifre"
+                      type="password"
+                      value={formik.values.password}
+                      onChange={formik.handleChange}
+                      onBlur={formik.handleBlur}
+                      name="password"
+                    />
+                    {formik.errors.password && formik.touched.password && (
+                      <div className={"ui pointing red basic label"}>
+                        {formik.errors.password}
+                      </div>
+                    )}
                   </div>
-                )}
-              </div>
-              <div style={{ marginTop: "1em" }}>
-                <label><b>Şifre Tekrar</b></label>
-                <Form.Input
-                  fluid
-                  icon="lock"
-                  iconPosition="left"
-                  placeholder="Şifre tekrar"
-                  type="password"
-                  value={formik.values.rePassword}
-                  onChange={formik.handleChange}
-                  onBlur={formik.handleBlur}
-                  name="rePassword"
-                />
-                {formik.errors.rePassword && formik.touched.rePassword && (
-                  <div className={"ui pointing red basic label"}>
-                    {formik.errors.rePassword}
+                  <div style={{ marginTop: "1em" }}>
+                    <label><b>Şifre Tekrar</b></label>
+                    <Form.Input
+                      fluid
+                      icon="lock"
+                      iconPosition="left"
+                      placeholder="Şifre tekrar"
+                      type="password"
+                      value={formik.values.rePassword}
+                      onChange={formik.handleChange}
+                      onBlur={formik.handleBlur}
+                      name="rePassword"
+                    />
+                    {formik.errors.rePassword && formik.touched.rePassword && (
+                      <div className={"ui pointing red basic label"}>
+                        {formik.errors.rePassword}
+                      </div>
+                    )}
                   </div>
-                )}
-              </div>
-            </Grid.Column>
-          </Grid>
+                </Grid.Column>
+              </Grid>
 
-          <br />
-          <Button color="teal" fluid size="large" type="submit">
-            Kayıt Ol
-          </Button>
-        </Segment>
-      </Form>
-      <Message info><Link to={"/registerEmployer"}><b>İşveren olarak kaydolmak için buraya tıkla</b></Link></Message>
+              <br />
+              <Button color="teal" fluid size="large" type="submit">
+                Kayıt Ol
+              </Button>
+            </Segment>
+          </Form>
+          <Message info><Link to={"/registerEmployer"}><b>İşveren olarak kaydolmak için buraya tıkla</b></Link></Message>
+        </div>}
     </div>
   );
 }
